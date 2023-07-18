685e8f8dbf8eb1c680ea2c85f3b324cc0f5a4f12
PaolaaR <p.riveranovoa@gmail.com>
2023-07-17 23:35:04 -0400

    last sections HTML

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-function addData(event) {
-    event.preventDefault();
+// Valida los datos ingresados en el formulario antes de enviarlos a la tabla
+
+function validateForm() {
     let nombre = document.getElementById("input-nombre").value;
     let apellido = document.getElementById("input-apellido").value;
     let email = document.getElementById("input-email").value;
 
-  
-    let usersList;
-    if (localStorage.getItem("usersList") === null) {
-        usersList = [];
-     } else {
-        usersList = JSON.parse(localStorage.getItem("usersList"));
-     }
-        usersList.push({nombre, apellido, email});
-        localStorage.setItem("usersList", JSON.stringify(usersList));
-        showData();
+    if (nombre === "" || apellido === "" || email === "") {
+        alert("Por favor, complete todos los campos");
+        return false;
+    }
+    return true;
 }
+
+// Muestra los datos en la tabla
 
 function showData() {
     let usersList;
@@ -23,23 +21,48 @@ function showData() {
      } else {
         usersList = JSON.parse(localStorage.getItem("usersList"));
      }
-     let tableBody = document.getElementById("travelers-list");
-     tableBody.innerHTML = "";
-     for (let i = 0; i < usersList.length; i++) {
-         let nombre = usersList[i].nombre;
-         let apellido = usersList[i].apellido;
-         let email = usersList[i].email;
-         tableBody.innerHTML += `
-         <tr>
-            <td>${nombre}</td>
-            <td>${apellido}</td>
-            <td>${email}</td>
-            <td><button class="btn btn-warning" onclick="editData(${i})">Editar</button></td>
-            <td><button class="btn btn-danger" onclick="deleteData(${i})">Eliminar</button></td>
-         </tr>
-         `;
-     }
+
+     var html= "";
+
+     usersList.forEach((user, index) => {
+        html += "<tr>";
+        html += "<td>" + user.nombre + "</td>";
+        html += "<td>" + user.apellido + "</td>";
+        html += "<td>" + user.email + "</td>";  
+        html += "<td><button class='btn btn-warning' onclick='editData(" + index + ")'>Editar</button></td>";
+        html += "<td><button class='btn btn-danger' onclick='deleteData(" + index + ")'>Eliminar</button></td>";
+        html += "</tr>";
+     });    
+     
+     document.getElementById("travelers-list").innerHTML = html;
+}
+
+//Añade los datos ingresados en el formulario a la tabla
+
+function addData() {
+   if (validateForm() == true) {
+    let nombre = document.getElementById("input-nombre").value;
+    let apellido = document.getElementById("input-apellido").value;
+    let email = document.getElementById("input-email").value;
+
+    let usersList;
+    if (localStorage.getItem("usersList") === null) {
+        usersList = [];
+    }else {
+        usersList = JSON.parse(localStorage.getItem("usersList"));
     }
+
+    usersList.push({nombre:nombre, apellido:apellido, email:email});
+
+    localStorage.setItem("usersList", JSON.stringify(usersList));
+    showData();
+    document.getElementById("input-nombre").value = "";
+    document.getElementById("input-apellido").value = "";
+    document.getElementById("input-email").value = "";
+    }
+}
+
+// Edita los datos ingresados en la tabla
 
     function editData(index) {
         document.getElementById('add-btn').style.display = 'none';
@@ -47,41 +70,49 @@ function showData() {
     
         let usersList;
         if (localStorage.getItem("usersList") === null) {
-            usersList = []
+            usersList = [];
         } else {
-            usersList = JSON.parse(localStorage.getItem("usersList"))
+            usersList = JSON.parse(localStorage.getItem("usersList"));
         }
-        document.querySelector('#input-nombre').value = usersList[index].nombre;
-        document.querySelector('#input-apellido').value = usersList[index].apellido;
-        document.querySelector('#input-email').value = usersList[index].email;
+        document.getElementById('input-nombre').value = usersList[index].nombre;
+        document.getElementById('input-apellido').value = usersList[index].apellido;
+        document.getElementById('input-email').value = usersList[index].email;
     
         document.getElementById('edit-btn').onclick = function () {
-            usersList[index].nombre = document.querySelector('#input-nombre').value
-            usersList[index].apellido = document.querySelector('#input-apellido').value
-            usersList[index].email = document.querySelector('#input-email').value
-    
+        if (validateForm ()== true) {
+            usersList[index].nombre = document.getElementById('input-nombre').value;
+            usersList[index].apellido = document.getElementById('input-apellido').value;
+            usersList[index].email = document.getElementById('input-email').value;
+
             localStorage.setItem("usersList", JSON.stringify(usersList));
+            
             showData();
-            document.querySelector('#input-nombre').value = ""
-            document.querySelector('#input-apellido').value = ""
-            document.querySelector('#input-email').value = ""
+            
+            document.getElementById('input-nombre').value = "";
+            document.getElementById('input-apellido').value = "";
+            document.getElementById('input-email').value = "";
     
             document.getElementById('add-btn').style.display = 'block';
             document.getElementById('edit-btn').style.display = 'none';
         }
-
     }
+
+}
+
+// Elimina los datos ingresados en la tabla
 
     function deleteData(index) {
         let usersList;
         if (localStorage.getItem("usersList") === null) {
-            usersList = []
+            usersList = [];
         } else {
             usersList = JSON.parse(localStorage.getItem("usersList"))
         }
-        usersList.splice(index, 1)
+        usersList.splice(index, 1);
         localStorage.setItem("usersList", JSON.stringify(usersList));
         showData();
-    }
+        }
 
-document.onload = showData();
+    // Carga de los datos de la pagina
+    
+    document.onload = showData();
